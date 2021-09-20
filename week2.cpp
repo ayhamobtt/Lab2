49b8041c49f4013e6e6d974b944b4f67058a0628
aeham <ayham.obtt@gmail.com>
2021-09-20 13:04:00 -0400

    initial commit

--- week2.cpp
+++ week2.cpp
@@ -35,15 +35,27 @@ protected:
 class MySecondClass { };
 
 
+
+
+
+
+
+
+
+
+
+
 #include"iostream"
 
 using namespace std;
 
 class Vehicle
+	// initlaizing two integer varibales by private defualt access
 {
 	int numWheels;
 	int numDoors;
 
+	//creating the first public method, that contaire 3 types of constractore and one deconstractore
 public:
 	Vehicle(int w, int d);
 	Vehicle(int w);
@@ -51,36 +63,41 @@ public:
 	~Vehicle();
 };
 
+// this first method/constractor talks no parameters 
 Vehicle::Vehicle()
 {
 	Vehicle(4); //A nameless local object will be created by calling Vehicle(int)
 	cout << "Constructor with 0 parameter" << endl;
 
 }
-
+// this second method/constractor talks one integer parameters 
 Vehicle::Vehicle(int w)
 {
 	Vehicle(4, w); //A nameless local object will be created by calling Vehicle(int,int)
 	cout << "Constructor with 1 parameter, where wheel=" << w << endl;
 }
 
+// this third method/constractor talks two integer parameters 
 Vehicle::Vehicle(int w, int d)
 {
+	// and store them in w,d that present numWheels,numDoors
 	numWheels = w;
 	numDoors = d;
 	cout << "Constructor with 2 parameter" << endl;
 }
-
+//that is the deconstractore calling automatically fuction
 Vehicle::~Vehicle()
 {
 	cout << " “In destructo" << endl;
 }
 
+// the main method
 int main(int argc, char** argv)
 {
-	Vehicle myVehicle(4); Vehicle CST8219 ;
 	Vehicle myVehicle(4, 2); 
-	Vehicle myVehicle; CST8219; //An object will be created by calling Vehicle(), two intermediate object, total three objects
+	Vehicle myVehicle; 
+	//An object will be created by calling Vehicle(), two intermediate object, total three objects
+	Vehicle CST8219 = Vehicle(4);
 	cout << "I made a Vehicle" << endl;
 	return 0;
 }
