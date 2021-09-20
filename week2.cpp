87eb1c15123139e0d4c1966d537aac903f313e37
aeham <ayham.obtt@gmail.com>
2021-09-19 23:54:19 -0400

    !

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,84 @@
 
 #include<iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+
+class MyFirstClass { 
+private:
+	int age;
+	char* firstName, * lastName;
+
+public:
+	MyFirstClass() : MyFirstClass(0,NULL,NULL) {
+
+	}
+	MyFirstClass(int a, char* f, char* l) 
+	{
+		age = a;
+		firstName = f;
+		lastName = l;  
+	}
+
+	~MyFirstClass() {
+		int i = 0; i++;
+	}
+	int getAge() { return age; }
+	char* getFirstName() { return firstName; }
+	char* getlastName() { return lastName; }
+	
+protected: 
+	void setFirstName(char* f) { firstName = f; }
+	void setLastName(char* l) { lastName = l; }
+
+};
+
+class MySecondClass { };
+
+
+#include"iostream"
+
+using namespace std;
+
+class Vehicle
 {
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+	int numWheels;
+	int numDoors;
+
+public:
+	Vehicle(int w, int d);
+	Vehicle(int w);
+	Vehicle();
+	~Vehicle();
+};
+
+Vehicle::Vehicle()
+{
+	Vehicle(4); //A nameless local object will be created by calling Vehicle(int)
+	cout << "Constructor with 0 parameter" << endl;
+
+}
+
+Vehicle::Vehicle(int w)
+{
+	Vehicle(4, w); //A nameless local object will be created by calling Vehicle(int,int)
+	cout << "Constructor with 1 parameter, where wheel=" << w << endl;
+}
+
+Vehicle::Vehicle(int w, int d)
+{
+	numWheels = w;
+	numDoors = d;
+	cout << "Constructor with 2 parameter" << endl;
+}
+
+Vehicle::~Vehicle()
+{
+	cout << " “In destructo" << endl;
+}
+
+int main(int argc, char** argv)
+{
+	Vehicle myVehicle; //An object will be created by calling Vehicle(), two intermediate object, total three objects
+	cout << "I made a Vehicle" << endl;
+	return 0;
 }
